7ce1b265d7301e4d0db47e13f76aa439adfdcd4f
scholtenskyle <35465569+scholtenskyle@users.noreply.github.com>
2024-11-17 12:47:09 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function binarySearch(list, element) {
-    return -1;
+    var listBeg = 0;
+    var listEnd = (list.length - 1);
+
+    var lowNum = -1;
+    
+    while (listBeg <= listEnd){
+        var listCenter = Math.floor((listBeg + listEnd) / 2);
+        if (element <= list[listCenter]){
+            listEnd = listCenter - 1;
+        }
+        if (list[listMid] == element){
+            lowNum = listCenter;
+        }
+        else if (element > list[listCenter]){
+            listBeg = listMid + 1;
+        }
+    }
+    return lowNum;
 }
